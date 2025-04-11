739b7f488a1468dd344cb056dc488ed521eb177c
kiritosshxd <96794616+kiritosshxd@users.noreply.github.com>
2025-04-10 21:54:42 -0300

    Create dnstt-installer.sh

---
+++ Slowdns/dnstt-installer.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+BIN_NAME="dnstt-manager"
+BIN_URL="https://github.com/kiritosshxd/SSHPLUS/raw/refs/heads/main/Slowdns/dnstt-manager"
+
+echo "[+] Baixando $BIN_NAME..."
+curl -sSL "$BIN_URL" -o "$BIN_NAME"
+
+if [ ! -f "$BIN_NAME" ]; then
+    echo "[ERRO] Falha ao baixar o binário."
+    exit 1
+fi
+
+chmod +x "$BIN_NAME"
+sudo mv "$BIN_NAME" /usr/local/bin/
+rm dnstt-installer.sh
+
+echo "[✔] Instalação completa! Você pode executar com:"
+echo "     dnstt-manager"
